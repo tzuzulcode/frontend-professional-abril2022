eaf3e7243a9da48bb519ed8f6f50f42f0fbde400
Tzuzul Code <mail@tzuzulcode.com>
2022-04-22 22:06:02 -0500

    Configuración de firebase

---
+++ ejemplo/config/firebase.js
@@ -0,0 +1,16 @@
+import {initializeApp} from 'firebase/app'
+
+
+const firebaseConfig = {
+    apiKey:process.env.APIKEY,
+    authDomain:process.env.AUTHDOMAIN,
+    projectId:process.env.PROJECTID,
+    storageBucket:process.env.STORAGEBUCKET,
+    messagingSenderId:process.env.MESSAGESENDERID,
+    appId:process.env.APPID,
+    measurementId:process.env.MEASUREMENTID
+}
+
+const app = initializeApp(firebaseConfig);
+
+export default app
